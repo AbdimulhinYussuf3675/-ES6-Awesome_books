5426c68956e10087dc048584a499735ac110dc5c
AbdimulhinYussuf3675 <adamabdimulhi.001@gmail.com>
2022-11-08 17:46:03 +0300

    Imported module

---
+++ index.js
@@ -0,0 +1,11 @@
+/* add nav SPA */
+import bookLink from './modules/nav.js';
+
+/* add book */
+import appendBook from './modules/addBooks.js';
+
+/* add book */
+import addRemoveListener from './modules/removeBooks.js';
+
+/* add time and date */
+import dateAndTime from './modules/time.js';
